92e6fd2ea3ff71c7096da49c82c960358762935a
Eddio0141 <eddio0141@gmail.com>
2024-04-20 13:18:12 +0100

    set up dev env

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = { flake-parts, nixpkgs, ... } @ inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+      ];
+      perSystem = { self', inputs', system, pkgs, ... }:
+      {
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            android-studio
+          ];
+
+          ANDROID_HOME = "/home/yuu/Android/Sdk";
+        };
+      };
+    };
+}
